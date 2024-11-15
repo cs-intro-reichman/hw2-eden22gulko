13299f587a0d110faf463e4336dbd485bfcff843
eden22gulko <eden.sokolovgulko@post.runi.ac.il>
2024-11-14 21:32:57 -0800

    exercise 5

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,41 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
-}
+	int times = Integer.parseInt(args[0]);
+	String vc = args[1];
+	String steps; 
+	
+	
+	for (int i = 1; i <= times ; i++ ){
+		int num = i; 
+		int count = 1;
+		steps = "";
+
+		do{
+			steps += num + " ";
+			if ((num % 2) == 0){
+				num = num / 2;
+			}else{
+				num = (num * 3) + 1;
+			}
+
+		count++;
+	
+		}while (num != 1);
+		
+		steps += num + " (" + count + ")";
+		if (vc.equals("v")){
+			System.out.println(steps);
+		}
+	 }
+
+	 System.out.println("Every one of the first " + times + " hailstone sequences reached 1.");
+	 
+    }
+
+  }
+
+  
+
+  
+	
