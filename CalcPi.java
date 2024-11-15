14a126fa86b2091a862089443701ee95f0b5e722
eden22gulko <eden.sokolovgulko@post.runi.ac.il>
2024-11-14 19:18:00 -0800

    fixing ex 4

--- CalcPi.java
+++ CalcPi.java
@@ -2,20 +2,21 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    int num = Integer.parseInt(args[0]);
-		double even = 0.0;
-		double odd = 0.0;
+		double pi = 0.0;
+		double bottom = 1.0;
 
 		for (int i = 0; i < num; i++){
+			double fraction = 1.0 / bottom;
 			if ((i % 2) == 0){
-				even = even + (1.0 / ((2 * i) + 1));
+				pi = pi + fraction;
 			}else{
-				odd = odd + (1.0 / ((2 * i) + 1));
+				pi = pi - fraction;
 			}
-
+			bottom += 2.0;
 		}
-		double approximated = 4 * (even - odd);
+		pi = pi * 4.0;
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated:     " + approximated);
+		System.out.println("pi, approximated:     " + pi);
 
 	}
 }
