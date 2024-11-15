a3e4767b8ef9edb3becab476db6b36352839b6e6
eden22gulko <eden.sokolovgulko@post.runi.ac.il>
2024-11-14 18:53:23 -0800

    exercise 4

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		double even = 0.0;
+		double odd = 0.0;
+
+		for (int i = 0; i < num; i++){
+			if ((i % 2) == 0){
+				even = even + (1.0 / ((2 * i) + 1));
+			}else{
+				odd = odd + (1.0 / ((2 * i) + 1));
+			}
+
+		}
+		double approximated = 4 * (even - odd);
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + approximated);
+
 	}
 }
